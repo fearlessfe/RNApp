bf7a2eee5f911dfb7e5c81120cc08f797703a5d1
pengzhen <pengzhen@zhigui.com>
2019-09-10 16:14:07 +0800

    fix:navigationBar

--- js/expand/dao/DataStore.js
+++ js/expand/dao/DataStore.js
@@ -1,4 +1,4 @@
-import {AsyncStorage} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import GitHubTrending from 'GitHubTrending';
 
 export const FLAG_STORAGE = {
